316be0e1320e2170a394d208fbfd2d993cade157
Jan Gosmann <jan.gosmann@tngtech.com>
2022-12-01 08:50:06 +0100

    Solve day 1 part 2

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,23 @@
 use std::io::{self, BufRead};
+use std::collections::BinaryHeap;
 
 fn main() {
     let stdin = io::stdin();
     
-    let mut max_calories_carried: u64 = 0;
+    let mut sorted_calories = BinaryHeap::new();
     let mut calories_carried: u64 = 0;
 
     for line in stdin.lock().lines().map(Result::unwrap) {
         if line.trim().is_empty() {
-            if calories_carried > max_calories_carried {
-                max_calories_carried = calories_carried;
-            }
+            sorted_calories.push(calories_carried);
             calories_carried = 0;
         } else {
             calories_carried += line.parse::<u64>().unwrap();
         }
     }
-    if calories_carried > max_calories_carried {
-        max_calories_carried = calories_carried;
-    }
+    sorted_calories.push(calories_carried);
     
+    let max_calories_carried = sorted_calories.pop().unwrap();
     println!("{}", max_calories_carried);
+    println!("{}", max_calories_carried + sorted_calories.pop().unwrap() + sorted_calories.pop().unwrap());
 }
